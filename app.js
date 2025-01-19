e3423015c2d90e15c3b5e8eb602ffe492d7e9c48
VartikaSharma <vartika.sharma@salescode.ai>
2025-01-19 11:57:35 +0530

    changes

--- app.js
+++ app.js
@@ -49,9 +49,11 @@ app.use('/', require("./routes/upload"));
 
 // static assets
 
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-  app.get('*' ,(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 
